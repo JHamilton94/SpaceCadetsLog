840535b184c0d536769349aa04a882d309520523
BuildTools <unconfigured@null.spigotmc.org>
2016-07-05 22:19:51 -0400

    Converted html files to jade, installed jade, and did something to the css file... cant remember what.

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
-
+app.set('view engine', 'jade');
 //db and post initialization
 var db = require('./mongo.js');
 var post;
-
+var currentBlogPage = 0;
 
 app.use(express.static('public'));
 
@@ -20,28 +21,31 @@ var server = app.listen(8080, function(){
 })
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + publicDirectory + pageDirectory + '/home.html');
+  res.render(__dirname + publicDirectory + pageDirectory + '/home.jade', {message: "asdf"});
   console.log("Request for home page");
 })
 
 app.get('/blog', function(req, res){
-  res.sendFile(__dirname + publicDirectory + pageDirectory + '/blog.html');
-  console.log("Request for blog page");
-  console.log("Page num: " + req.query.page);
+  //Page information
+  currentBlogPage = parseInt(req.query.page);
+  var olderPage = currentBlogPage + 1;
+  var newerPage = currentBlogPage - 1;
+ 
+  //gather posts to render
+  
+
+  //Send page assembled with posts
+  res.render(__dirname + publicDirectory + pageDirectory + '/blog.jade', {olderLink: "/blog?page=" + olderPage, newerLink: "/blog?page=" + newerPage});
+  console.log("Request for blog page: " + currentBlogPage);
 })
 
 app.get('/games', function(req, res){
-  res.sendFile(__dirname + publicDirectory + pageDirectory + '/games.html');
+  res.render(__dirname + publicDirectory + pageDirectory + '/games.jade');
   console.log("Request for games page");
 })
 
 app.get('/contact', function(req, res){
-  res.sendFile(__dirname + publicDirectory + pageDirectory + '/contact.html');
+  res.render(__dirname + publicDirectory + pageDirectory + '/contact.jade');
   console.log("Request for contact page");
 })
-
-app.get('/image', function(req, res){
-  res.sendFile(__dirname + publicDirectory + imageDirectory + '/mercury.jpg');
-})
-
 
