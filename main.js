9cbd674282b7db5862d47f672609d2f3045ee9e8
BuildTools <unconfigured@null.spigotmc.org>
2016-07-07 17:47:35 -0400

    Whole lot of changes, added content section for blog, added mongo calls in main, mongo.js is unused, modified the get request for /blog to support a whole lot of queries, can now filter by tags and search bar works. Updated css and jade file for blog page so that content is prettier and fixed some css/jade issues caused by the other changes.

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ var express = require('express');
 var fs = require('fs');
 var app = express();
 app.set('view engine', 'jade');
+
 //db and post initialization
-var db = require('./mongo.js');
-var post;
 var currentBlogPage = 0;
+var mongoDB = require('mongodb');
+var mongoClient = mongoDB.MongoClient;
+var url = 'mongodb://localhost:27017/blog';
 
 app.use(express.static('public'));
 
@@ -17,6 +19,7 @@ var server = app.listen(8080, function(){
   var host = server.address().address
   var port = server.address().port
 
+  
   console.log("Server running and listening at: %s:%s", host, port);
 })
 
@@ -26,17 +29,45 @@ app.get('/', function(req, res){
 })
 
 app.get('/blog', function(req, res){
-  //Page information
-  currentBlogPage = parseInt(req.query.page);
-  var olderPage = currentBlogPage + 1;
-  var newerPage = currentBlogPage - 1;
- 
-  //gather posts to render
-  
-
-  //Send page assembled with posts
-  res.render(__dirname + publicDirectory + pageDirectory + '/blog.jade', {olderLink: "/blog?page=" + olderPage, newerLink: "/blog?page=" + newerPage});
-  console.log("Request for blog page: " + currentBlogPage);
+  //page
+  if(req.query.page != null){
+    //Determine page
+    console.log("Queried for page: " + req.query.page);
+    currentBlogPage = parseInt(req.query.page);
+    var olderPage = currentBlogPage + 1;
+    var newerPage = currentBlogPage - 1;
+    
+    //Find entries with tag
+    if(req.query.tag != null){
+         mongoClient.connect(url, function(err, db){
+         var cursor = db.collection('posts').find({'tags': req.query.tag}).limit(100).toArray(function(err, results){ 
+           
+           
+           res.render(__dirname + publicDirectory + pageDirectory + '/blog.jade', {olderLink: "/blog?page=" 
+             + olderPage + "&tag=" + req.query.tag, newerLink: "/blog?page=" + newerPage + "&tag=" + req.query.tag, postContent: results});
+           console.log("Request for blog page: " + currentBlogPage + " tag: " + req.query.tag);
+         });
+       });
+    //Find entries with search
+    }else if(req.query.search !=  null){
+       searchTerm = '\.*'+req.query.search+'\.';
+       mongoClient.connect(url, function(err, db){
+         var cursor = db.collection('posts').find({'content': new RegExp(searchTerm, 'i')}).limit(100).toArray(function(err, results){ 
+           res.render(__dirname + publicDirectory + pageDirectory + '/blog.jade', {olderLink: "/blog?page=" 
+             + olderPage + "&search=" + req.query.search, newerLink: "/blog?page=" + newerPage + "&search=" + req.query.search, postContent: results});
+           console.log("Request for blog page: " + currentBlogPage + " search: " + req.query.search);
+         });
+       });
+    //Find chronological entries
+    }else{
+      mongoClient.connect(url, function(err, db){
+        var cursor = db.collection('posts').find().limit(100).toArray(function(err, results){
+          res.render(__dirname + publicDirectory + pageDirectory + '/blog.jade', {olderLink: "/blog?page=" + olderPage, newerLink: "/blog?page=" + newerPage, postContent: results});
+          console.log("Request for blog page: " + currentBlogPage);
+        });
+      });
+    }
+  }
 })
 
 app.get('/games', function(req, res){
--- mongo.js
+++ mongo.js
@@ -5,16 +5,55 @@ var mongoDB = require('mongodb');
 var mongoClient = mongoDB.MongoClient;
     
 //The URL
-var url = 'mongodb://localhost:27017/blog_posts';
+var url = 'mongodb://localhost:27017/blog';
     
 var exports = module.exports = {};
- 
 
+exports.posts = [];
 exports.num = 7;
+
+//Returns a set number of posts
+exports.getPosts = function(pageNum){ 
+
+  mongoClient.connect(url, function(err, db){
+    //establish connection
+    if(err){
+      console.log('error connecting to database ERROR: ' + err);
+    }else{
+      console.log('connected to db successfully');
+    }
+    
+    var i = 0;
+
+    var collection = db.collection('posts').find();    
+
+    collection.each(function(err, doc){
+      if(err){
+        console.log('ERROR: ' + err);
+      }else{
+        console.log('Success I guess?');
+      }
+
+      if(doc != null){
+        exports.posts.push(doc);
+        i++;
+      }else{
+        db.close();
+      }
+    });
+
+    for(var i = 0; i < 15; i++){
+      console.log("Array: " + i + " " + exports.posts[i]);
+    } 
+
+    //var returnCollection = collection.find(); //.sort({date:1}).limit(100);
+  });
+
+  return exports.posts;  
+}
 
 exports.insertPost = function(){
 
- 
   mongoClient.connect(url, function(err, db){
     if(err){
       console.log("Shits fucked yo: ", err);
@@ -24,13 +63,19 @@ exports.insertPost = function(){
 
     var collection = db.collection('posts');
 
-    var post1 = {title: "script insert", content: "Script written content", date: new Date('2016-04-01'), tags: ['second', 'script'], by: "JROD"};
+    var arr = [];
+    arr.length = 15;
 
-    collection.insert(post1, function(err, result){
+    for(var i = 0; i < 15; i++){
+      console.log("Loop: " + i);
+      arr[i] = {title: "title #" + i, content: "Content #" + i, date: new Date('2016-04-' + i), tags: ['Cow' + i, 'Sheep' + i], by: 'author #' + i};
+    }
+    
+    collection.insert(arr, function(err, result){
       if(err){
-        console.log("error inserting post1: " + err);
+        console.log("error inserting post ERROR: " + err);
       } else {
-        console.log("inserted post 1 successfully");
+        console.log("inserted post successfully");
       }
       db.close();
     });
